64ac1602e7ede54e7d35f09e4bcdfa8c4579fe9a
Nematjon <frank.sport.93@gmail.com>
2019-05-20 11:32:13 +0300

    scripts.js added

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// Base concepts in JavaScript.
+// variable declaration
+var someValue = "String literal"; // variable with string literal
+let letMeDeclareVariable = "block scope"; // block scope variable
+const constantDeclaration = "Can't change primitive value"; // constant declaration
+
+// Function keyword
+function someFunction(param) {  // function declaration
+  return `It has some parameter: ${param}`;
+}
+
+// if block
+if(true) {  // if block statement
+  return "It is true";
+}
