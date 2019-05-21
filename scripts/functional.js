1bf187ab75bc981f70120ffa470dfccb9749561b
Nematjon <frank.sport.93@gmail.com>
2019-05-21 11:37:35 +0300

    updated functional.js file

--- scripts/functional.js
+++ scripts/functional.js
@@ -44,4 +44,44 @@ function zipCode(code, location) {
     }
   };
 }
+// const princetonZip = zipCode("08544", "3345");
+// proncetonZip.toString(); |# "08544-3345"
+var person = Object.freeze(new Person("Haskell", "Curry", "444-44-4444"));
+person.firstname = "Bob"; // Not allowed
+var isObject = val => val && typeOf val === "object";
+function deepFreeze(obj) {
+  if(isObject(obj) &&
+      !Object.isFrozen(obj)) {
+    Object.keys(obj).forEach(name => deepFreeze(obj[name]));
+    Object.freeze(obj);
+  }
+  return obj;
+}
+// Ramda.js library
+var person = new Person("Alonzo", "Church", "444-44-4444");
+var lastnameLens = R.lenseProp("lastname");
+R.view(lastnameLens, person); // |# "Church"
+var newPerson = R.set(lastnameLens, "Mourning", person);
+newPerson.lastname;// |# "Mourning"
+person.lastname; // |# "Church"
+var zipPath = ["address", "zip"];
+var zipLens = R.lens(R.path(zipPath), R.assocPath(zipPath));
+R.view(zipLens, person); // |# zipCode("08544", "1234")
+var newPerson = R.set(zipLens, person, zipCode("90210", "5678"));
+R.view(zipLens, newPerson); // |# zipCode("90210", "5678")
+R.view(zipLens, person);// |# zipCode("08544", "1234")
+
+//============= higher-order functions
+function applyOperator(a, b, opt) {
+  return opt(a, b);
+}
+var multiplier = (a, b) => a * b;
+applyOperation(2, 3, multiplier); // |# 6
+
+function add(a) {
+  return function(b) {
+    return a + b;
+  }
+}
+add(3)(3); // |# 6
 
