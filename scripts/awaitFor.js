ed8f4fd58bc22f1b3853fe42c49162d39fff78a2
Nematjon <frank.sport.93@gmail.com>
2019-05-25 12:09:24 +0300

    for await loop

---
+++ scripts/awaitFor.js
@@ -0,0 +1,28 @@
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums"
+];
+
+const getData = async function() {
+  try {
+    const [users, posts, albums] = await Promise.all(urls.map(async function(url) {
+      const response = await fetch(url);
+      return response.json();
+    }));
+    console.log("users", users);
+    console.log("posts", posts);
+    console.log("albums", albums);
+  } catch(err) {
+    console.log("ooooops", err)
+  }
+};
+
+const getData = async function() {
+  const arrayOfPromises = urls.map(url => fetch(url));
+  for await (let request of arrayOfPromises) {
+    const data =  await request.json();
+    console.log(data);
+  }
+};
+
